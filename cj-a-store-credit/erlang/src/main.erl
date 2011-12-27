bf9358c347821d16b85cc4ee02c13a305d3348f6
Muharem Hrnjadovic <mh@foldr3.com>
2011-12-27 15:12:34 +0100

    added module doc string

--- cj-a-store-credit/erlang/src/main.erl
+++ cj-a-store-credit/erlang/src/main.erl
@@ -1,3 +1,14 @@
+%% @author Muharem Hrnjadovic
+%% @doc Solves the code jam practice problem described here:
+%%          http://code.google.com/codejam/contest/dashboard?c=351101#s=p0
+%%
+%%      You receive a credit C at a local store and would like to buy
+%%      two items. You first walk through the store and create a
+%%      list L of all available items. From this list you would like to
+%%      buy two items that add up to the entire value of the credit. The
+%%      solution you provide will consist of the two integers indicating
+%%      the positions of the items in your list (smaller number first).
+
 -module(main).
 -export([main/1, printer/3]).
 -import(input, [process_data/2]).
