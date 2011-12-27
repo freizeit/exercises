d817fdc22c26c3494d93aef7f4dc23850c7d7dbf
Muharem Hrnjadovic <mh@foldr3.com>
2011-12-27 12:28:17 +0100

    added result printer

--- cj-a-store-credit/erlang/src/main.erl
+++ cj-a-store-credit/erlang/src/main.erl
@@ -12,18 +12,25 @@
 %% doc The main() function, triggers the calculation.
 -spec main(Path :: string()) -> done.
 main(Path) ->
-    Rcvr = spawn(?MODULE, printer, [fun io:format/1, -1, 1]),
+    Maxint = lists:last(erlang:system_info(heap_sizes)),
+    Rcvr = spawn(?MODULE, printer, [fun io:format/1, Maxint, 1]),
     input:handle_file(Path, Rcvr),
     done.
 
 
-%% doc Prints the results.
--spec printer(Fun :: function(), Max :: integer(), Count :: integer()) -> done.
-printer(_, N, N) -> done;
-printer(Fun, Max, Count) ->
-    receive
-        {max, V} -> printer(Fun, V, Count);
-        {res, S} -> Fun(S), printer(Fun, Max, Count+1)
+%% doc Prints the results. The actual number of results is
+%% communicated via the {count, C} message.
+-spec
+    printer(Fun :: function(), Count :: integer(), Idx :: integer())
+    -> integer().
+printer(Fun, Count, Idx) ->
+    if
+        Idx =< Count ->
+            receive
+                {count, C} -> printer(Fun, C, Idx);
+                {res, S} -> Fun(S), printer(Fun, Count, Idx+1)
+            end;
+        true -> Count
     end.
 
 
@@ -41,8 +48,24 @@ printer(Fun, Max, Count) ->
 % -----------------------------------------------------------------------------
 % Tests for printer()
 % -----------------------------------------------------------------------------
-printer_exit_condition_test() ->
-    ?assertEqual(done, printer(fun erlang:exit/1, 11, 11)).
-
-printer_exit_condition_test() ->
+printer_terminates_test() ->
+    Me = self(),
+    FakeFun = fun(S) -> Me ! S end,
+    P = spawn(?MODULE, printer, [FakeFun, 33, 1]),
+    timer:sleep(50),
+    P ! { res, "R1" },
+    P ! { res, "R3" },
+    P ! { count, 3 },
+    P ! { res, "R2" },
+    receive "R1" -> ?assert(true)
+    after 1000 -> ?assert(false)
+    end,
+    receive "R3" -> ?assert(true)
+    after 1000 -> ?assert(false)
+    end,
+    receive "R2" -> ?assert(true)
+    after 1000 -> ?assert(false)
+    end,
+    timer:sleep(50),
+    ?assert(not is_process_alive(P)).
 -endif.
