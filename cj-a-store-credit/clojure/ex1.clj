80f87ab214b11d118faeab3135d3a2445a25ed0e
Muharem Hrnjadovic <mh@foldr3.com>
2012-05-08 23:30:19 +0200

    code works, validation passes, http://code.google.com/codejam/contest/dashboard?c=351101#s=p0

--- cj-a-store-credit/clojure/ex1.clj
+++ cj-a-store-credit/clojure/ex1.clj
@@ -1,17 +1,28 @@
 (ns ex1
   (:import (java.io FileReader BufferedReader)))
 
+
+(defn sum-prices
+  "Sums up two prices and ignores their indices."
+  [ip1 ip2]
+  (let [[_ p1] ip1 [_ p2] ip2]
+    (+ p1 p2)))
+
+
 (defn find-match
   "Finds two prices whose sum equals the credit and returns these as a vector.
-   Otherwise returns nil"
+   Otherwise returns nil."
   [c ps]
   (if (< (count ps) 2)  ;; at least two prices are needed for a solution
     nil ;; no solution
     (let [fp (first ps) rps (rest ps)
-          remainder (seq (drop-while #(not (= (+ fp %) c)) rps))]
+          remainder (seq (drop-while #(not (= (sum-prices fp %) c)) rps))]
       (if (not (nil? remainder))
-          [fp (first remainder)]  ;; we have a solution
-          (recur c rps)))))       ;; drop the 1st price and try again
+          ;; we have a solution, return the 1-based indices
+          (let  [[i1 _] fp [i2 _] (first remainder)]
+            [(inc i1) (inc i2)])
+          ;; no solution for this price, try the remaining ones
+          (recur c rps)))))
 
 (defn solve
     "Finds a solution for a block of 3 lines that hold the store credit,
@@ -23,18 +34,29 @@
           credit (. Integer parseInt l1)  ;; convert to int
           ;; split the string with the prices and convert the latter to int
           prices (map #(. Integer parseInt %) (re-seq #"\d+" l3))
-          solution (find-match credit prices)]
-      (println "idx     : " idx)
-      (println "credit  : " credit)
-      (println "prices  : " prices)
-      (println "solution: " solution))
-    )
+          ;; each price needs an index since only the latter is to appear in
+          ;; the solution
+          solution (find-match credit (map-indexed vector prices))]
+      ;;(println "idx     : " idx)
+      ;;(println "credit  : " credit)
+      ;;(println "prices  : " prices)
+      ;;(println "solution: " solution)
+      (if (nil? solution)
+        (format "Case #%d: no solution found" idx)
+        (let [[i1 i2] solution]
+          (format "Case #%d: %d %d" idx i1 i2)))
+    ))
 
 (defn -main [path]
   (let [_fr (new FileReader path)
-        br (rest (line-seq (new BufferedReader _fr)))] ;; ignore 1st line
+        br (rest (line-seq (new BufferedReader _fr))) ;; ignore 1st line
     ;; iterate through the lines in blocks of 3; also pass a 1-based task index
     ;; to the solve() function. The index will be used to format the result.
-    (doall (map solve (iterate inc 1) (partition 3 br))))
+        os (doall (map solve (iterate inc 1) (partition 3 br)))]
+    (loop [lines os]
+      (if (not (nil? lines))
+        (let [h (first lines) t (rest lines)]
+          (println h)
+          (recur (seq t))))))
     (shutdown-agents)
   )
