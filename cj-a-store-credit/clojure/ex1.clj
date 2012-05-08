ab89362c2ed4cb849c1d1606251bc54ad2de831a
Muharem Hrnjadovic <mh@foldr3.com>
2012-05-08 23:47:54 +0200

    works with larger jobs, doall hangs

--- cj-a-store-credit/clojure/ex1.clj
+++ cj-a-store-credit/clojure/ex1.clj
@@ -52,8 +52,8 @@
         br (rest (line-seq (new BufferedReader _fr))) ;; ignore 1st line
     ;; iterate through the lines in blocks of 3; also pass a 1-based task index
     ;; to the solve() function. The index will be used to format the result.
-        os (doall (map solve (iterate inc 1) (partition 3 br)))]
-    (loop [lines os]
+        output (map solve (iterate inc 1) (partition 3 br))]
+    (loop [lines output]
       (if (not (nil? lines))
         (let [h (first lines) t (rest lines)]
           (println h)
