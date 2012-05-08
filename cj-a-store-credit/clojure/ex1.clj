8c5a4bd5b00f14ebee6b54151cc903f47aed0f02
Muharem Hrnjadovic <mh@foldr3.com>
2012-05-08 18:52:29 +0200

    work in progress

--- cj-a-store-credit/clojure/ex1.clj
+++ cj-a-store-credit/clojure/ex1.clj
@@ -8,16 +8,25 @@
     (. q put result)
     result))
 
-(defn process-input [path]
-  (let [fr (new FileReader path)
-        br (rest (seq (line-seq (new BufferedReader fr))))]
-    (take 3 br))
-  )
+(defn find [t pl]
+  (let [fp (first pl) ps (rest pl)
+        remainder (drop-while #(not (= (+ fp %) t)) ps)]
+    (if (not (nil? remainder))
+        [fp (first remainder)]
+        (recur t ps))))
+
+(defn process-input [bo3]
+    (let [[l1 l2 l3] bo3
+          total (. Integer parseInt l1)
+          prices (map #(. Integer parseInt %) (re-seq #"\d+" l3))]
+      (println "total is: " total)
+      (println "prices is: " prices)
+      (println "solution: " (find total prices))))
 
 (defn -main [path]
-  (let [q (new LinkedBlockingQueue)
-        results (pmap calc [(range 10 15) (range 20 25)] [q q])]
-    (println q path)
-    (println (process-input path)))
+  (let [oq (new LinkedBlockingQueue)
+        _fr (new FileReader path)
+        br (rest (line-seq (new BufferedReader _fr)))]
+    (println (map process-input (partition 3 br))))
     (shutdown-agents)
   )
