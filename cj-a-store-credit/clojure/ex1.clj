5c035d1e5029d91c255455b960e424a552b83549
Muharem Hrnjadovic <mh@foldr3.com>
2012-05-08 12:16:41 +0200

    work in progress

--- cj-a-store-credit/clojure/ex1.clj
+++ cj-a-store-credit/clojure/ex1.clj
@@ -1,5 +1,6 @@
 (ns ex1
-  (:import (java.util.concurrent LinkedBlockingQueue)))
+  (:import (java.util.concurrent LinkedBlockingQueue))
+  (:import (java.io FileReader BufferedReader)))
 
 (defn calc [vs q]
   (let [result (reduce + vs)]
@@ -7,9 +8,16 @@
     (. q put result)
     result))
 
+(defn process-input [path]
+  (let [fr (new FileReader path)
+        br (rest (seq (line-seq (new BufferedReader fr))))]
+    (take 3 br))
+  )
+
 (defn -main [path]
   (let [q (new LinkedBlockingQueue)
         results (pmap calc [(range 10 15) (range 20 25)] [q q])]
-    (println q path))
-    (. System exit 0)
+    (println q path)
+    (println (process-input path)))
+    (shutdown-agents)
   )
