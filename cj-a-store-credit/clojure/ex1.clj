3d219148ba2da4c943ff44bffe43ffd55631be93
Muharem Hrnjadovic <mh@foldr3.com>
2012-05-09 13:22:35 +0200

    parallel execution

--- cj-a-store-credit/clojure/ex1.clj
+++ cj-a-store-credit/clojure/ex1.clj
@@ -52,7 +52,7 @@
         br (rest (line-seq (new BufferedReader _fr))) ;; ignore 1st line
     ;; iterate through the lines in blocks of 3; also pass a 1-based task index
     ;; to the solve() function. The index will be used to format the result.
-        output (map solve (iterate inc 1) (partition 3 br))]
+        output (pmap solve (iterate inc 1) (partition 3 br))]
     (loop [lines output]
       (if (not (nil? lines))
         (let [h (first lines) t (rest lines)]
