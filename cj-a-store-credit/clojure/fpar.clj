e297aaae38bc9a0d44e8210ee228126bd525b5fb
Muharem Hrnjadovic <mh@foldr3.com>
2012-05-13 10:13:51 +0200

    another minor fix

--- cj-a-store-credit/clojure/fpar.clj
+++ cj-a-store-credit/clojure/fpar.clj
@@ -8,8 +8,8 @@
         br (rest (line-seq (new BufferedReader _fr))) ;; ignore 1st line
     ;; iterate through the lines in blocks of 3; also pass a 1-based task index
     ;; to the solve() function. The index will be used to format the result.
-        fs (for [args (map vector (iterate inc 1) (partition 3 br))] (future (calc/vsolve args)))]
-    (loop [outstanding (doall fs)]
+        futures (for [args (map vector (iterate inc 1) (partition 3 br))] (future (calc/vsolve args)))]
+    (loop [outstanding (seq (doall futures))]
       (if (not (nil? outstanding))
         (let [[done, not-done] (map seq (split-with future-done? outstanding))]
           ;;(println "done     : " done)
