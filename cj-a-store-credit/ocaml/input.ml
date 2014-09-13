241943a1c2fc29ada2ba97504a56a9ea0cf34d93
Muharem Hrnjadovic <mh@cloud1.it>
2014-09-13 10:14:58 +0200

    add documentation for Input module

--- cj-a-store-credit/ocaml/input.ml
+++ cj-a-store-credit/ocaml/input.ml
@@ -1,7 +1,17 @@
+(** Logic needed to read "Store Credit" files. Please @see <https://code.google.com/codejam/contest/dashboard?c=351101#s=p0> the google code jam page for more details *)
+
 open Core.Std
 
 
-let rec read_N_lines_ file n blen lines =
+(** Reads a block of lines from a "Store Credit" file
+   @param file an open channel to read from
+   @param n remaining number of lines to read
+   @param total total number of lines to read
+   @param lines list collecting the lines read so far
+   @return a list of strings where each element is a line read from the file
+   @raise Failure if a complete block of lines could not be read
+ *)
+let rec read_N_lines' file n total lines =
   if n = 0 then List.rev lines
   else
     let maybe_line = In_channel.input_line file in
@@ -10,17 +20,29 @@ let rec read_N_lines_ file n blen lines =
       (* end of input? *)
       if lines = [] then lines
       else (* input cut off in the middle of a n-line block *)
-        let msg = "Failed to read " ^ string_of_int blen ^ "-line block" in
+        let msg = "Failed to read " ^ string_of_int total ^ "-line block" in
         raise (Failure msg)
     | Some line ->
-      read_N_lines_ file (n - 1) blen (line :: lines)
+      read_N_lines' file (n - 1) total (line :: lines)
 
 
+(** Reads a block of lines from a "Store Credit" file
+   @see "read_N_lines'" for more detail
+   @param file an open channel to read from
+   @param n remaining number of lines to read
+   @return list of strings where each element is a line read from the file
+   @raise Failure if the block size is zero or less
+ *)
 let read_N_lines file n =
   if n <= 0 then raise (Failure "block length must be > 0")
-  else read_N_lines_ file n n []
+  else read_N_lines' file n n []
 
 
+(** Processes all records in a "Store Credit" file while keeping track of the
+  1-based index of the records processed.
+   @param file an open channel to read from
+   @param n 1-based index of the store credit record to process
+ *)
 let rec process_lines' file n =
   let block = read_N_lines file 3 in
   if block = [] then
@@ -32,9 +54,15 @@ let rec process_lines' file n =
   end
 
 
-let process_lines file =
-  process_lines' file 1
+(** Processes all records in a "Store Credit" file
+   @param file an open channel to read from
+ *)
+let process_lines file = process_lines' file 1
 
+
+(** Open a "Store Credit" file and make sure all records are processed
+   @param path path of the "Store Credit" file to be processed
+ *)
 let process_file path () =
   In_channel.with_file path ~f:(fun file ->
     (* ignore first line *)
